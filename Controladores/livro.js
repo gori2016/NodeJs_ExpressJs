fd582db862f1f1f07f394678c1992204af3668db
Gori2016 <igorborgesaraujo1677@gmail.com>
2026-02-26 10:15:39 -0300

    Atualizar controlador de livros para retornar lista de livros do arquivo JSON e remover arquivo de teste

--- Controladores/livro.js
+++ Controladores/livro.js
@@ -1,6 +1,13 @@
+ 
+ //modulo fs é responsável por ler, criar, atualizar, deletar e escrever arquivos, ele é nativo do node, ou seja, não precisa ser instalado
+ const fs = require("fs")
+
+
+ // essa parte do código faz parte do controlador do livro é aqui que estão as funções que vão ser executadas quando as rotas forem acessadas no app.js
  function getLivros (req, res){
     try {
-        res.send("Olá alura do Brasil")
+        const livros = JSON.parse(fs.readFileSync('livros.json'))
+        res.send(livros)
     } catch (error) {
         res.status(500)
         res.send(error.message)
