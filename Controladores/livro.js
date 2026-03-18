ce69a2ffb23bc6131a3793dcf2aedd92863a8d76
Gori2016 <igorborgesaraujo1677@gmail.com>
2026-03-18 09:17:32 -0300

    Adicionar validação de ID e nome nas funções de obter, inserir, modificar e deletar livros; atualizar arquivo JSON com novo livro

--- Controladores/livro.js
+++ Controladores/livro.js
@@ -19,8 +19,14 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = Number(req.params.id)
-        const livro = getLivroPorId(id)
-        res.send(livro)
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("Id inválido, por favor insira um id válido")
+        }
+
 
         //tratamento de erros
     } catch (error) {
@@ -32,9 +38,17 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send("Livro Inserido com Sucesso")
+        if (req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro Inserido com Sucesso")
+        } else {
+            res.status(422)
+            res.send("Nome invalido")
+        }
+
+
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -44,29 +58,41 @@ function postLivro(req, res) {
 function patchLivro(req, res) {
     try {
         const id = Number(req.params.id)
-        const body = req.body
-        modificaLivro(body, id)
-        res.send("Livro Modificado com sucesso")
+        if (id && Number(id)) {
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Livro Modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido, por favor insira um id válido")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
 
-function apagarLivro(req, res){
+function apagarLivro(req, res) {
     try {
         const id = Number(req.params.id)
-        const body = req.body
-        deleteLivro(id,body)
-        res.send("Livro Deletado com Sucesso")
+        if (id && Number(id)) {
+            const body = req.body
+            deleteLivro(id, body)
+            res.send("Livro Deletado com Sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido, por favor insira um id válido")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
-    
 
 
-    module.exports = {
-        getLivros, getLivro, postLivro, patchLivro, apagarLivro
-    }
+
+module.exports = {
+    getLivros, getLivro, postLivro, patchLivro, apagarLivro
+}
