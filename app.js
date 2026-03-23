8bc70cd8a7b68f4fdd99612099c31cf6b37d1867
gori2016 <igorborgesaraujo1677@gmail.com>
2026-03-22 22:10:55 -0300

    Adicionar middleware CORS, atualizar dependências e arquivos JSON com novos livros e favoritos

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //importa o express, cria o servidor
 const express = require("express")
-
+const cors = require("cors")
 //Importa o arquivo aonde foi criado as rotas do livro. aqui está sendo exportado o router 
 const rotaLivro = require("./Rotas/livro.js")
+
 
 //cria a aplicação
 const app = express()
@@ -11,6 +12,7 @@ const port = 8000
 
 //isso significa sempre que alguém acessar algo que comece com /livros, use as rotas do arquivo livro.js
 app.use(express.json()) // isso é necessário para que o express consiga ler o corpo da requisição, sem isso o req.body vai ser undefined
+app.use(cors({origin:"*"}))
 app.use('/livros', rotaLivro)
 
 app.listen(port,() =>{
